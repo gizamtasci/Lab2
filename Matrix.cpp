477c0a7263e5e733e9495ba8be615b825d6b7406
gizamtasci <50792712+gizamtasci@users.noreply.github.com>
2019-10-14 21:57:27 +0300

    Create Matrix.cpp

---
+++ Matrix.cpp
@@ -0,0 +1,39 @@
+#include "Matrix.h"
+#include <iostream>
+#include <iomanip>
+using namespace std;
+void Matrix_Allocate(Matrix& matrix, int rowSize, int columnSize) {
+	// LOOK AT Problem Solving Tips Element (1)
+}
+void Matrix_Free(Matrix& matrix) {
+	// LOOK AT Problem Solving Tips Element (2)
+}
+void Matrix_FillByValue(Matrix& matrix, float value) {
+	// LOOK AT Problem Solving Tips Element (3)
+}
+void Matrix_FillByData(Matrix & matrix, float ** data) {
+	// LOOK AT Problem Solving Tips Element (4)
+}
+void Matrix_Display(const Matrix& matrix) {
+	// LOOK AT Problem Solving Tips Element (5)
+}
+void Matrix_Addition(const Matrix & matrix_left, const Matrix & matrix_right,
+	Matrix & result) {
+	// LOOK AT Problem Solving Tips Element (6)
+}
+void Matrix_Substruction(const Matrix & matrix_left,
+	const Matrix & matrix_right, Matrix & result) {
+	// LOOK AT Problem Solving Tips Element (7)
+}
+void Matrix_Multiplication(const Matrix & matrix_left,
+	const Matrix & matrix_right, Matrix & result) {
+	// LOOK AT Problem Solving Tips Element (8)
+}
+void Matrix_Multiplication(const Matrix & matrix_left, float scalarValue,
+	Matrix & result) {
+	// LOOK AT Problem Solving Tips Element (9)
+}
+void Matrix_Division(const Matrix & matrix_left, float scalarValue,
+	Matrix & result) {
+	// LOOK AT Problem Solving Tips Element (10)
+}
